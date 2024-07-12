f26274f6e7ba501d4ef8ee42a9ce8e934f9e0cac
isaac0yen <isaacoyeniyi06@gmail.com>
2024-07-12 14:02:19 +0100

    server

--- server/index.js
+++ server/index.js
@@ -6,8 +6,19 @@ const Logger = require("./helpers/Logger");
 const { mySQLConnect } = require("./helpers/Database");
 
 const userRoutes = require("./routes/user.routes");
+const courseRoutes = require("./routes/course.routes");
+const departmentRoutes = require("./routes/department.routes");
+const facultyRoutes = require("./routes/faculty.routes");
+const timetableRoutes = require("./routes/timetable.routes");
+const accountRoutes = require("./routes/account.routes");
 
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/department", departmentRoutes);
+app.use("/api/v1/faculty", facultyRoutes);
+app.use("/api/v1/room", roomRoutes);
+app.use("/api/v1/timetable", timetableRoutes);
+app.use("/api/v1/account", accountRoutesRoutes);
 
 Logger.init();
 
